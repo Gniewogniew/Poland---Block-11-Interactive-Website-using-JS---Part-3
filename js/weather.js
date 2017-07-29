b790a19d5536d02bf5c241f7c890bedbf843ada9
Gniewogniew <jakubbajer83@gmail.com>
2017-07-29 16:57:03 +0200

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,37 +1,34 @@
-if("geolocation" in navigator){
-	navigator.geolocation.getCurrentPosition(function(position){
-		loadWeather(position.coords.latitude + " , " + position.coords.longitude);
+$("geolocation").ready(function() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    loadWeather(position.coords.latitude+','+position.coords.longitude); 
+    setInterval(loadWeather, 3600000);
 	});
-}else {
-	loadWeather("Szczecin, PL", "");
 
-}
+	function loadWeather(location, woeid){
+		$.simpleWeather({
+			location: location,
+			woeid: woeid,
+			unit: 'c',
+			success: function(weather){
+				city = weather.city + ',' + weather.region;
+				temp = weather.temp +" &deg;" + weather.units.temp;
+				wcode = '<img class="weathericon" src="images/weathericon/'+weather.code+'.svg">';
+				wind = "<p>" + weather.wind.speed + "</p><p>" + weather.units.speed + "</p>";
+				humidity = weather.humidity + " %";
+				sunrise = weather.sunrise;
+				sunset = weather.sunset;
 
-$(document).ready(function() {
-
-	setInterval(loadWeather, 10000);
-});
-
-function loadWeather(position, woeid){
-	$.simpleWeather({
-		location: location,
-		woeid: woeid,
-		unit: "c",
-		success: function(weather){
-			city = weather.city;
-			temp = weather.temp +" &deg;";
-			wcode = "<img class = weathericon src=images/weathericons/ + weather.code + .svg>";
-			wind = "<p>" + weather.wind.speed + "</p><p>" + weather.units.speed + "</p>";
-			humidity = weather.humidity + "&";
-			
-			$(".location").text(city);
-			$(".temperature").html(temp);
-			$(".climate_bg").html(wcode);
-			$(".windspeed").html(wind);
-			$(".humidity").text(humidity);	
-		}, 
-		error: function(error){
-			$(".error").html("<p>" + error + "</p>");	
-		}
-	});
-}
+				$(".location").text(city);
+				$(".temperature").html(temp);
+				$(".climate_bg").html(wcode);
+				$(".windspeed").html(wind);
+				$(".humidity").text(humidity);	
+				$(".sunrise").text(sunrise);
+				$(".sunset").text(sunset);
+			},
+			error: function(error){
+				$(".error").html("<p>" + error + "</p>");	
+			}
+		});
+	}
+})
