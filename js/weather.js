974c7acfb409b0b473c007ee7b64102ea3c2454c
Gniewogniew <jakubbajer83@gmail.com>
2017-07-30 12:18:46 +0200

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,8 @@
+function attachEventHandlers (){
+	$("#submit").click(weatherAroundTheWorld);
+	$("#check-weather-button").click(weatherAroundTheWorld);
+	$("#check-weather-input").keypress(inputTextClickHandler)
+}
 $("geolocation").ready(function() {
   navigator.geolocation.getCurrentPosition(function(position) {
     loadWeather(position.coords.latitude+','+position.coords.longitude); 
@@ -32,3 +37,43 @@ $("geolocation").ready(function() {
 		});
 	}
 })
+
+var weatherAroundTheWorld = function(event){
+	var city=(event.target)
+	var inputText = (event.target.previousElementSibling).value;
+	$(".error").html("");
+	$.simpleWeather({
+			location: (inputText),
+			woeid: '',
+			unit: 'c',
+			success: function(weather){
+				city = weather.city + ',' + weather.region;
+				temp = weather.temp +" &deg;" + weather.units.temp;
+				wcode = '<img class="weathericon" src="images/weathericons/'+weather.code+'.svg">';
+				wind = "<p>" + weather.wind.speed + "</p><p>" + weather.units.speed + "</p>";
+				humidity = weather.humidity + " %";
+				sunrise = weather.sunrise;
+				sunset = weather.sunset;
+
+				$(".location").text(city);
+				$(".temperature").html(temp);
+				$(".climate_bg").html(wcode);
+				$(".windspeed").html(wind);
+				$(".humidity").text(humidity);	
+				$(".sunrise").text(sunrise);
+				$(".sunset").text(sunset);
+			},
+			error: function(error){
+				$(".error").html("<p>" + error + "</p>");	
+			}
+		});
+	$("#check-weather-input").val("");
+}
+
+var inputTextClickHandler = (function(event) {
+        if (event.which == 13) {
+            $("#check-weather-button").click();
+    }
+});
+
+$(attachEventHandlers)
