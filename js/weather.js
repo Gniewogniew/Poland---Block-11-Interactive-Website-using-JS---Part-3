b4122cac1b5d09c36084dfcba345af6b81b25d3d
Gniewogniew <jakubbajer83@gmail.com>
2017-07-31 18:33:53 +0200

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,17 +1,22 @@
 function attachEventHandlers (){
+	$("geolocation").ready(geolocationHandler);
 	$("#submit").click(weatherAroundTheWorld);
 	$("#check-weather-button").click(weatherAroundTheWorld);
-	$("#check-weather-input").keypress(inputTextClickHandler)
+	$("#check-weather-input").keypress(inputTextClickHandler);
+	
 }
-$("geolocation").ready(function() {
-  navigator.geolocation.getCurrentPosition(function(position) {
+
+var geolocationHandler = function(event){
+ 	navigator.geolocation.getCurrentPosition(function(position) {
     loadWeather(position.coords.latitude+','+position.coords.longitude); 
     setInterval(loadWeather, 3600000);
-	});
+	})
+}
 
-	function loadWeather(location, woeid){
+
+function loadWeather(location, woeid){
 		$.simpleWeather({
-			location: location,
+			location: location, globalinputText,
 			woeid: woeid,
 			unit: 'c',
 			success: function(weather){
@@ -36,37 +41,14 @@ $("geolocation").ready(function() {
 			}
 		});
 	}
-})
 
+var globalinputText;
 var weatherAroundTheWorld = function(event){
-	var city=(event.target)
+	var city = (event.target)
 	var inputText = (event.target.previousElementSibling).value;
+	globalinputText = input;
 	$(".error").html("");
-	$.simpleWeather({
-			location: (inputText),
-			woeid: '',
-			unit: 'c',
-			success: function(weather){
-				city = weather.title;
-				temp = weather.temp +" &deg;" + weather.units.temp;
-				wcode = '<img class="weathericon" src="images/weathericons/'+weather.code+'.svg">';
-				wind = "<p>" + weather.wind.speed + "</p><p>" + weather.units.speed + "</p>";
-				humidity = weather.humidity + " %";
-				sunrise = weather.sunrise;
-				sunset = weather.sunset;
-
-				$(".location").text(city);
-				$(".temperature").html(temp);
-				$(".climate_bg").html(wcode);
-				$(".windspeed").html(wind);
-				$(".humidity").text(humidity);	
-				$(".sunrise").text(sunrise);
-				$(".sunset").text(sunset);
-			},
-			error: function(error){
-				$(".error").html("<p>" + error + "</p>");	
-			}
-		});
+	$(loadWeather)
 	$("#check-weather-input").val("");
 }
 
