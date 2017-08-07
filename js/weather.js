aa455d937ddbca5be04edc6810b3e75c9562437c
Gniewogniew <jakubbajer83@gmail.com>
2017-08-07 18:54:55 +0200

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,58 +1,58 @@
-$(function(){ 
+function showUp(){
+    geolocation();
+    getWeatherFromUserInput()
 
-function attachEventHandlers (){
-	$("#cityValue").keypress(function(event){
-		if (event.which == 13) {
-            	$("#submitCity").click();
-    		}
-	});
-}
+    function getWeatherFromUserInput() {
+        $("#cityValue").keypress(function(event) {
+            if (event.which === 13); {
+                $("#submitCity").click();
+            }
+        });
+        $("#submitCity").click(function() {
+            $(".error").html("");
+            loadWeather($("#cityValue").val());
+            $("#cityValue").val("");
+        });
+    }
 
-var geolocation = function(event){
- 	navigator.geolocation.getCurrentPosition(locationFromGoelocation, defaultLocation);
+	function geolocation(event){
+ 		navigator.geolocation.getCurrentPosition(locationFromGoelocation, defaultLocation);
  	}
-		function locationFromGoelocation(position){
-	    	loadWeather(position.coords.latitude+','+position.coords.longitude);
-		setInterval(geolocation, 3600000);
-		}
-		function defaultLocation(position) {
+	function locationFromGoelocation(position){
+	    loadWeather(position.coords.latitude+','+position.coords.longitude);
+		setTimeout(geolocation, 3600000);
+	}
+	function defaultLocation(position){
 	  	loadWeather(52.22967560 + ',' + 21.01222870);
-	  	setInterval(geolocation, 3600000);
-	  	}
-
-function loadWeather(location, woeid){
-		$.simpleWeather({
-			location: location,
-			woeid: woeid,
-			unit: 'c',
-			success: function(weather){
-				city = weather.title;
-				temp = weather.temp +" &deg;" + weather.units.temp;
-				wcode = '<img class="weathericon" src="images/weathericons/'+weather.code+'.svg">';
-				wind = "<p>" + weather.wind.speed + "</p><p>" + weather.units.speed + "</p>";
-				humidity = weather.humidity + " %";
-				sunrise = weather.sunrise;
-				sunset = weather.sunset;
-
-				$(".location").text(city);
-				$(".temperature").html(temp);
-				$(".climate_bg").html(wcode);
-				$(".windspeed").html(wind);
-				$(".humidity").text(humidity);	
-				$(".sunrise").text(sunrise);
-				$(".sunset").text(sunset);
-			},
-			error: function(error){
-				$(".error").html("<p>" + error + "</p>");	
-			}
-		});
+	  	setTimeout(geolocation, 3600000);
 	}
 
-$("#submitCity").click(function(){
-	$(".error").html("");
-	loadWeather($("#cityValue").val());
-	$("#cityValue").val("");
-});
-$(geolocation)
-$(attachEventHandlers)
-});
+    function loadWeather(location, woeid) {
+        $.simpleWeather({
+            location: location,
+            woeid: woeid,
+            unit: 'c',
+            success: function(weather) {
+                city = weather.title;
+                temp = weather.temp + " &deg;" + weather.units.temp;
+                wcode = '<img class="weathericon" src="images/weathericons/' + weather.code + '.svg">';
+                wind = "<p>" + weather.wind.speed + "</p><p>" + weather.units.speed + "</p>";
+                humidity = weather.humidity + " %";
+                sunrise = weather.sunrise;
+                sunset = weather.sunset;
+
+                $(".location").text(city);
+                $(".temperature").html(temp);
+                $(".climate_bg").html(wcode);
+                $(".windspeed").html(wind);
+                $(".humidity").text(humidity);
+                $(".sunrise").text(sunrise);
+                $(".sunset").text(sunset);
+            },
+            error: function(error) {
+                $(".error").html("<p>" + error + "</p>");
+            }
+        });
+    }
+}
+$(showUp)
